346bf226b2fa3489f5a780b00e001850c6c4e93e
jesgeo <jesgeo>
2020-05-21 12:52:46 +0100

    add pubsub entry point

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ function sampleNodeBackend(req,res) {
 function samplePubSub(event, context) {
   const pubsubMessage = event.data;
   console.log(Buffer.from(pubsubMessage, 'base64').toString());
+  console.log(event);
+  console.log(context);
+
 };
 
 
